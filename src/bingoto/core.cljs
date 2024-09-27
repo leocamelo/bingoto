150395fc4598066c1ccf874bbf8598e7a94fc899
leocamelo <leonardocamelo.nave@gmail.com>
2024-09-26 21:32:36 -0300

    add bingo75 factory algorithm

--- src/bingoto/core.cljs
+++ src/bingoto/core.cljs
@@ -11,7 +11,7 @@
 (defn app []
   [:div.container.mx-auto.text-center
    [:h1.text-3xl.font-semibold.my-4 "Welcome to Bingoto"]
-   [comps/bingo75-card (bingo75/values)]])
+   [comps/bingo75-card (bingo75/gen-values (js/Date.now))]])
 
 ;; -------------------------
 ;; Initialize app
--- src/bingoto/factories/bingo75.cljs
+++ src/bingoto/factories/bingo75.cljs
@@ -1,5 +1,40 @@
-(ns bingoto.factories.bingo75)
+(ns bingoto.factories.bingo75
+  (:require
+   ["pure-rand" :as prand]))
 
-(defn values []
-  (for [i (range 25)]
-    (str (inc i))))
+(def balls-len 75)
+(def balls (for [i (range balls-len)] (str (inc i))))
+
+(def cols-len 5)
+(def rows-len 5)
+
+(def values-len (* cols-len rows-len))
+(def balls-group-len (/ balls-len cols-len))
+
+(defn- assoc-value [values col row index]
+  (assoc values (+ col (* row cols-len)) (nth balls index)))
+
+(defn- gen-column-values
+  ([values rng min-index max-index col]
+   (gen-column-values values rng min-index max-index col 0 #{}))
+
+  ([values rng min-index max-index col row memo]
+   (if (= row rows-len)
+     values
+     (let [[i, rng] (prand/uniformIntDistribution min-index max-index rng)]
+       (if (contains? memo i)
+         (recur values rng min-index max-index col row memo)
+         (recur (assoc-value values col row i) rng min-index max-index col (inc row) (conj memo i)))))))
+
+(defn- do-gen-values [values rng]
+  (reduce
+   #(let [min-index (* %2 balls-group-len)
+          max-index (+ min-index (dec balls-group-len))]
+      (gen-column-values %1 rng min-index max-index %2))
+   values
+   (range cols-len)))
+
+(defn gen-values [seed]
+  (let [rng (prand/xoroshiro128plus seed)
+        values (vec (repeat values-len ""))]
+    (assoc (do-gen-values values rng) (/ values-len 2) "")))
